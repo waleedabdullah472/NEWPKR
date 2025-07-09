dcf95b01dd782ae26d9c277cc36c900ba9556507
Fahad SEO <fahadwaxeem007@gmail.com>
2025-07-09 05:58:51 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-RE2K0LT6T1";
+ReactGA.initalize(TRACKING_ID);
+useEffect(()=>{
+  pageView(loaction.pathname + LOCATION>SEARCH);
+},[location]);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
