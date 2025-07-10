352df90c5f846afb62303600fe8ad2be2f1483d3
aryaniqbal <162159197+aryaniqbal@users.noreply.github.com>
2025-07-10 11:29:43 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ReactGA from "react-ga";
-const TRACKING_ID = "G-RE2K0LT6T1";
-ReactGA.initalize(TRACKING_ID);
-useEffect(()=>{
-  pageView(loaction.pathname + LOCATION>SEARCH);
-},[location]);
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
