f7dd64893942d072887d761f0a14b7dca38dfb76
WinK899 <fahadwaxeem007@gmail.com>
2025-07-17 02:08:34 -0700

    vip event & inivite code link,logo link ,keywords added in it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,28 +34,59 @@ const Header = () => {
 
       {/* Navigation Section */}
       <div className="nav-section">
-   <div className="logo">
-    <img src={Logo} alt="Logo" className="logo-img" />
-  </div>
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="Logo" className="logo-img" />
+          </Link>
+        </div>
 
-        
+
         {/* Mobile Menu Button */}
         <button className="mobile-menu-btn" onClick={toggleMobileMenu}>
           {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
         </button>
-        
+
+
         {/* Navigation Links - Desktop */}
         <div className="nav-options">
-          <Link to="/">VIP Events</Link>
-          <Link to="/">Official Invite Code</Link>
+          <a
+            href="https://www.92pkrs.com/#/register?invitationCode=28742968539"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            VIP Events
+          </a>
+          <a
+            href="https://www.92pkrs.com/#/register?invitationCode=28742968539"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Official Invite Code
+          </a>
+
           <Link to="/blogs">Blogs</Link>
           <Link to="/contact-us">Contact Us</Link>
         </div>
-        
+
+
         {/* Mobile Menu */}
         <div className={`mobile-menu ${isMobileMenuOpen ? 'open' : ''}`}>
-          <Link to="/" onClick={toggleMobileMenu}>VIP Events</Link>
-          <Link to="/" onClick={toggleMobileMenu}>Official Invite Code</Link>
+          <a
+            href="https://www.92pkrs.com/#/register?invitationCode=28742968539"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={toggleMobileMenu}
+          >
+            VIP Events
+          </a>
+          <a
+            href="https://www.92pkrs.com/#/register?invitationCode=28742968539"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={toggleMobileMenu}
+          >
+            Official Invite Code
+          </a>
           <Link to="/blogs" onClick={toggleMobileMenu}>Blogs</Link>
           <Link to="/contact-us" onClick={toggleMobileMenu}>Contact Us</Link>
         </div>
